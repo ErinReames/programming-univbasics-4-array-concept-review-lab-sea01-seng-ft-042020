e7f0f2de547a33f1596274bb6a111b7bff331caf
Erin Reames <italiagirl83@gmail.com>
2020-03-20 15:35:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,4 +21,11 @@ end
 
 def find_min_value(array)
   # Add your solution here
+  x = 0
+  array.length.times do |index|
+    if array[index] < x
+      x array[index]
+    end
+  end
+  return x 
 end
